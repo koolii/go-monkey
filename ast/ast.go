25e5a54d24f8c5370108e7dbd71be64d20f28c5f
koolii <koolii0909@gmail.com>
2021-01-03 17:21:13 +0900

    Add new AST and a test for ast_test.go

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,10 @@
 package ast
 
-import "github.com/koolii/go-monkey/token"
+import (
+	"bytes"
+
+	"github.com/koolii/go-monkey/token"
+)
 
 // 結局の所ASTを作成し、木構造となる
 
@@ -12,6 +16,7 @@ import "github.com/koolii/go-monkey/token"
 // TokenLiteral()はテスト・デバッグ用で利用する
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // statementNode()はダミーでExpressionインターフェイス
@@ -40,6 +45,15 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+// バッファを作成、それぞれのXXXStatementのString()メソッドの戻り値をバッファに書き込む
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // LetStatement let x = 10: などの文のNode
 type LetStatement struct {
 	Token token.Token // token.Let
@@ -49,6 +63,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // Identifier 変数名の確保のためのNode(識別子)
 // token.IDENTのTypeはIDENT、Literalは実際の変数名文字列
@@ -62,6 +91,7 @@ type Identifier struct {
 // そもそもIdentifierはNodeの種類を少なくするため、変数束縛の名前を表現のために作っている
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type ReturnStatement struct {
 	Token       token.Token // token.RETURN
@@ -70,3 +100,31 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal } // return
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement 式文でProgram.Statementsに追加できる
+type ExpressionStatement struct {
+	Token      token.Token // 式の最初のトークン
+	Expression Expression  // 式を保存
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
